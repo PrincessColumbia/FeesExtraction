bfbc18bb2284e4a4f6746284b16bba62cc68cbb0
Christy McFarland <PrincessColumbia@users.noreply.github.com>
2017-06-21 15:31:50 -0700

    Some sort of int bug
    Successfully uses human input to haarvest some information, but the iteration function to autogenerate rows in a table won't properly read the integers.

--- conversion2.ps1
+++ conversion2.ps1
@@ -9,6 +9,7 @@ if ($detectOS -eq "Unix") {
 # Project home directory
 $projectHome = $HOME + $dirChar + "Desktop" +$dirChar + "FeesExtraction" + $dirChar
 $projectTemp = $projectHome + "temp" + $dirChar
+$projectResources = $projectHome + "Resources"
 $summaryRoutedAudit = $projectHome + "Audit" + $dirChar + "Summary Routed" + $dirChar
 
 # Today's Date (In short format)
@@ -45,73 +46,14 @@ if ($startList) {
     }
 
 
+# Create the imported data array from the starting list of documents to process
 $importDataSet = Import-Csv $startListPath
-
-<#NOTE -
-
-Move most (or ALL) of these fields to a new table object
-
-#>
 
 $importDataSet | Add-Member -MemberType NoteProperty -Name CityOrEntity -Value $null
 $importDataSet | Add-Member -MemberType NoteProperty -Name State -Value $null
 $importDataSet | Add-Member -MemberType NoteProperty -Name Assigned -Value $env:USERNAME
-# $importDataSet | Add-Member -MemberType NoteProperty -Name "Form Completed" -Value Schrodinger
-# $importDataSet | Add-Member -MemberType NoteProperty -Name Notes -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name "Link" -Value $null
 
-<# $importDataSet | Add-Member -MemberType NoteProperty -Name Downloaded -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name "Row Type" -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name lawson_infopro_polygon -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name is_cust_owned -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name serviceType -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name is_this_row_additional_cont -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name marketType -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name stop_cd -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name rate_type -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name district_cd -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name charge_cd -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name serv_freq -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name contract_nbr -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name contract_grp -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name quantity_threshold -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name "MIN quantity" -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name "MAX quantity" -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name "Rate applies per item" -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name size -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name cont_type -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name extra_unit_type -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name extra_units -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name extra_units_size -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name delivery -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name removal -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name serv_int_fee -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name cont_rep_fe -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name cont_exch_fee -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name Late_fee -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name other_fees -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name other_fees_desc -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name FRF -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name FRF_exempt_cd -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name ERF -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name ERF_exempt_cd -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name Admin -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name Admin_exempt_cd -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name service_reinst_fee -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name bill_freq -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name price -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name discounted_rate -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name save_rate -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name account_type -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name rev_dist_code -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name month_in_adv -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name ACQ_code -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name invoice_group -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name is_CRC_Consolidated -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name sold_as -Value $null
-$importDataSet | Add-Member -MemberType NoteProperty -Name bundle_id -Value $null
-# $importDataSet | Add-Member -MemberType NoteProperty -Name Notes -Value $null
-
-#>
 
 $divisionResourcesFile = $projectHome + "Resources" + $dirChar + "rs_divlist.csv"
 $divisionResourcesCSV = Import-Csv $divisionResourcesFile
@@ -122,23 +64,23 @@ $divisionResourcesCSV | Add-Member -MemberType NoteProperty -Name "Billing and C
 
 
 $divisionResourcesCSV | ForEach-Object {
-
- $_.Area = $_.Area -replace ': ','-'
-
-    $AreaWeb = $_.Area -replace ' ','%20'
+<# 
+    $_.Area = $_.Area -replace ': ','-'
+    $AreaWeb = $_.Area -replace '-','_'
+    $AreaWeb = $AreaWeb -replace ' ','%20'
     $AreaWebSub = $AreaWeb + "/"
     $areaSpaceUnderscore = $_.Area -replace ' ','_'
     $areaDashUnderscore = $_.Area -replace '-','_'
     $areaOnlyUnderscore = $areaSpaceUnderscore -replace '-','_'
     $AreaOnly = $_.Area.Substring(4)
     $AreaOnlyUnderscore = $areaSpaceUnderscore.Substring(4)
-
+#>
     $DataEntryName = "DIV_" + $_."Division #" + "_Data_Entry_Information.html"
     $BillingName = "DIV_" + $_."Division #" + "_Billing_and_Collection_Information.html"
 
     $compassWebLocationDiv = "http://compass.repsrv.com/DivisionalDocuments/"
-    $genDocsFileBilling = $compassWebLocationDiv + $AreaWeb + "/DIV_" + $_."Division #" + "_Billing_and_Collection_Information.html"
-    $genDocsFileDataEntry = $compassWebLocationDiv + $AreaWeb + "/" + $DataEntryName
+    $genDocsFileBilling = $compassWebLocationDiv + $_.Area + "/" + $BillingName
+    $genDocsFileDataEntry = $compassWebLocationDiv + $_.Area + "/" + $DataEntryName
     $_."Data Entry Link" = $genDocsFileDataEntry
     $_."Billing and Collections Link" = $genDocsFileBilling
     $_.DataEntryFileName = $DataEntryName
@@ -148,8 +90,7 @@ $divisionResourcesCSV | ForEach-Object {
 
 $importDataSet | ForEach-Object {
 
-    $_.Name = $_.Name -replace " ","_"
-    $_.Name = $_.Name -replace "__","_"
+    $_.Name = $_.Name -replace " ","%20"
 
     $rateId = $_.LawsonDiv + "-" + $_.DivisionNo + "-" + $_.PolygonID
     $_."Rate ID" = $rateId
@@ -180,15 +121,10 @@ Add-Content -Path $tempTable -Value 'DivNum,DataEntryFileName,DataEntryLink,Bill
 $divisionList | ForEach-Object {
 
     $divNoTemp = $_
-    $dataEntryFilenameTemp = $divisionResourcesCSV | Where-Object { $_."Division #" -eq $divNoTemp } | Get-Member -Name "DataEntryFileName"
-    $dataEntryFilenameTemp = $dataEntryFilenameTemp -split "=" | Where { $_ -notmatch "string" }
-    $divDataItem1 = $divisionResourcesCSV | Where-Object { $_."Division #" -eq $divNoTemp } | Get-Member -Name "Data Entry Link"
-    $divDataItem1 = $divDataItem1 -split "=" | Where { $_ -notmatch "string" }
-    $billingFilenameTemp = $divisionResourcesCSV | Where-Object { $_."Division #" -eq $divNoTemp } | Get-Member -Name "BillingFileName"
-    $billingFilenameTemp = $billingFilenameTemp -split "=" | Where { $_ -notmatch "string" }
-    $divDataItem2 = $divisionResourcesCSV | Where-Object { $_."Division #" -eq $divNoTemp } | Get-Member -Name "Billing and Collections Link"
-    $divDataItem2 = $divDataItem2 -split "=" | Where { $_ -notmatch "string" }
-
+    $dataEntryFilenameTemp = $divisionResourcesCSV | Where-Object { $_."Division #" -eq $divNoTemp } | Select-Object DataEntryFileName | Select -ExpandProperty "DataEntryFileName"
+    $divDataItem1 = $divisionResourcesCSV | Where-Object { $_."Division #" -eq $divNoTemp } | Select-Object "Data Entry Link" | Select -ExpandProperty "Data Entry Link"
+    $billingFilenameTemp = $divisionResourcesCSV | Where-Object { $_."Division #" -eq $divNoTemp } | Select-Object BillingFileName | Select -ExpandProperty "BillingFileName"
+    $divDataItem2 = $divisionResourcesCSV | Where-Object { $_."Division #" -eq $divNoTemp } | Select-Object "Billing and Collections Link" | Select -ExpandProperty "Billing and Collections Link"
 
     $rowToTemp = $divNoTemp + "," + $dataEntryFilenameTemp + "," + $divDataItem1 + "," + $billingFilenameTemp + "," + $divDataItem2
     Add-Content -Path $tempTable -Value $rowToTemp
@@ -200,6 +136,8 @@ $divisionListWithLinks = Import-Csv $tempTable
 $divisionListWithLinks | Add-Member -MemberType NoteProperty -Name "NumOfInv_Groups" -Value $null
 $divisionListWithLinks | Add-Member -MemberType NoteProperty -Name "Inv_Groups_Symbols" -Value $null
 
+# Clean up the temporary data
+Remove-Item -Path $tempTable -Force
 
 $divisionListWithLinks | ForEach-Object {
     
@@ -210,11 +148,12 @@ $divisionListWithLinks | ForEach-Object {
     [io.file]::WriteAllText($fileSaveLocation,(Invoke-WebRequest -URI $_.BillingLink -UseDefaultCredentials -UseBasicParsing).content)
 
     # logic to use user input to get the invoice groups goes here
-    start chrome $fileSaveLocationBilling
-    $countInvGroups = Read-Host -Prompt 'How many Invoice Groups are in Billing Information - Residential/Invoice Group for Division' $_.DivNum
+    start chrome $_.BillingLink
+    $countInvGroups = Read-Host -Prompt 'How many Invoice Groups are in Billing Information - Residential/Invoice Group for the Division (see the browser that just popped up)'
     $symbolsInvGroups = Read-Host -Prompt 'What are the invoice group symbols? (ex. A,B,C,1,2,3) Separate multiple symbols with commas'
     $_.NumOfInv_Groups = $countInvGroups
     $_.Inv_Groups_Symbols = $symbolsInvGroups
+    
 }
 
 
@@ -224,7 +163,16 @@ $trackerExportRefined = $projectHome + "RefinedExport.csv"
 
 $trackerExport = @()
 $importDataSet | ForEach-Object {
-
+    
+    # Getting the count of the container sizes
+    $countingOpen = $projectTemp + $_.Name
+    start chrome $_.Link
+    $countOfContainerSizes = Read-Host -Prompt 'How many container sizes are there?'
+    # Retrieving count from $divisionListWithLinks
+    $divCompare = $_.DivisionNo
+    $preCount = $divisionListWithLinks | Where-Object { $_.DivNum -eq $divCompare } | Select-Object NumOfInv_Groups | Select -ExpandProperty "NumOfInv_Groups"
+    $totalLinesPerCode = [int]$countOfContainerSizes * [int]$preCount
+    
     # $summaryRoutedValue = 0
     $summaryRoutedFile = $projectTemp + $_.Name
         # Solid Waste
@@ -297,6 +245,7 @@ $importDataSet | ForEach-Object {
     $trackerObject | Add-Member -MemberType NoteProperty -Name "PolygonID" -Value $_.PolygonID
     $trackerObject | Add-Member -MemberType NoteProperty -Name "Rate ID" -Value $_."Rate ID"
     $trackerObject | Add-Member -MemberType NoteProperty -Name "Area" -Value $_.Area
+    $trackerObject | Add-Member -MemberType NoteProperty -Name "TotalLines" -Value $totalLinesPerCode
     $tempFileExist = $projectTemp + $_.Name
     $fileExistTest = Test-Path $tempFileExist
     if ($fileExistTest) {
@@ -318,20 +267,89 @@ $importDataSet | ForEach-Object {
         $trackerObject | Add-Member -MemberType NoteProperty -Name "Form Completed" -Value $null
         $trackerObject | Add-Member -MemberType NoteProperty -Name "Notes" -Value $null
     }
+
     $trackerExport += $trackerObject
 }
 $trackerExport | Export-Csv -Path $trackerExportRefined -NoTypeInformation
 
-<#
-Area
-Link
-In Compass
-Form Completed
-Notes
+$rateIDList = $importDataSet."Rate ID" | Select-Object -Unique
+
+$dataExport = @()
+$rateIDList | ForEach-Object {
+    
+    $rateIDalpha = $_
+    $lineCount = $trackerExport | Where-Object { $_."Rate ID" -eq $rateIDalpha } | Select-Object TotalLines | Select -ExpandProperty "TotalLines"
+
+    for ($i = $lineCount, $i -ne 0, $i--) {
+    
+        $dataExportObject = New-Object PSObject
+        $dataExportObject | Add-Member -MemberType NoteProperty -Name "Rate_ID" -Value $_
+
+        $dataExport += $dataExportObject
+    }
+}
+        <# $importDataSet | Add-Member -MemberType NoteProperty -Name Downloaded -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name "Row Type" -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name lawson_infopro_polygon -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name is_cust_owned -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name serviceType -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name is_this_row_additional_cont -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name marketType -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name stop_cd -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name rate_type -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name district_cd -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name charge_cd -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name serv_freq -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name contract_nbr -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name contract_grp -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name quantity_threshold -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name "MIN quantity" -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name "MAX quantity" -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name "Rate applies per item" -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name size -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name cont_type -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name extra_unit_type -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name extra_units -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name extra_units_size -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name delivery -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name removal -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name serv_int_fee -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name cont_rep_fe -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name cont_exch_fee -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name Late_fee -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name other_fees -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name other_fees_desc -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name FRF -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name FRF_exempt_cd -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name ERF -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name ERF_exempt_cd -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name Admin -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name Admin_exempt_cd -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name service_reinst_fee -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name bill_freq -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name price -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name discounted_rate -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name save_rate -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name account_type -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name rev_dist_code -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name month_in_adv -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name ACQ_code -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name invoice_group -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name is_CRC_Consolidated -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name sold_as -Value $null
+$importDataSet | Add-Member -MemberType NoteProperty -Name bundle_id -Value $null
+# $importDataSet | Add-Member -MemberType NoteProperty -Name Notes -Value $null
+
 #>
+
+
+
+$dataExportFileLocation = $projectHome + "DataExport.csv"
+$dataExport | Export-Csv -Path $dataExportFileLocation -NoTypeInformation
+
 
 # Test the export, this is NOT the final product, that needs to be divided out by DIV
 $trackerTest = $projectHome + "tracker-test.csv"
-$compiledResults = $projectHome + "compiled-results.csv"
+#$compiledResults = $projectHome + "compiled-results.csv"
 $importDataSet | Select-Object Name,LawsonDiv,DivisionNo,PolygonID,"Rate ID",CityOrEntity,State,Assigned,"Form Completed",Notes | Export-Csv -Append $trackerTest -NoTypeInformation #$areaTrackerFilename
-$importDataSet | Select-Object "Row Type","Rate ID" | Export-Csv -Append -NoTypeInformation -Path $compiledResults
+#$importDataSet | Select-Object "Row Type","Rate ID" | Export-Csv -Append -NoTypeInformation -Path $compiledResults
